9d26826c95f2e7f0b452ddf7dc3780fc9338ecdf
Mike Nguyen <mike@toch.im>
2013-08-16 19:26:26 -0400

    Preparing for heroku push

--- catacombs/wsgi.py
+++ catacombs/wsgi.py
@@ -25,7 +25,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "catacombs.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+#per heroku's instruction
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
