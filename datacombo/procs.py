ce6ac613e932f5d23e5a4f8ab9f9492c9b9d7907
Mike Nguyen <mike@toch.im>
2013-09-12 11:57:39 -0400

    Modified procs.py to work with heroku

--- datacombo/procs.py
+++ datacombo/procs.py
@@ -1,8 +1,12 @@
 from hirefire.procs.rq import RQProc
 from redis import StrictRedis
+from urlparse import urlparse
+import os
+redistogo_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379/')
+u = urlparse(redistogo_url)
 
 
 class WorkerProc(RQProc):
     name = 'worker'
     queues = ['high', 'default', 'low']
-    connection = StrictRedis('localhost', 6379, db=0)
+    connection = StrictRedis(host=u.hostname, port=u.port, db=0)
