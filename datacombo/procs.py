45fc9b5297a4bba7213918ebffd8937c11c93746
Mike Nguyen <mike@toch.im>
2013-09-16 14:48:34 -0400

    Added db=0 to procs.py

--- datacombo/procs.py
+++ datacombo/procs.py
@@ -2,11 +2,11 @@ from hirefire.procs.rq import RQProc
 from redis import StrictRedis
 from urlparse import urlparse
 import os
-redis_url = os.getenv('REDISGREEN_URL', 'redis://localhost:6379/')
-u = urlparse(redis_url)
+redistogo_url = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379/')
+u = urlparse(redistogo_url)
 
 
 class WorkerProc(RQProc):
     name = 'worker'
     queues = ['high', 'default', 'low']
-    connection = StrictRedis(host=u.hostname, port=u.port, 0, password=u.password)
+    connection = StrictRedis(host=u.hostname, port=u.port, db=0, password=u.password)
